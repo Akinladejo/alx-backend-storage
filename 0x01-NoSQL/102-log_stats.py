9aafe4bd74d78460cd647197c2d55db59c78347a
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-10-24 16:40:56 +0100

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f'{status_check} status check')
+
+    pipeline = [
+        {"$match": {"ip": {"$ne": None}}},  # Ensure ip is not null
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+
+    print("IPs:")
+    for doc in nginx_collection.aggregate(pipeline):
+        ip = doc["_id"]
+        count = doc["count"]
+        print(f'\t{ip}: {count}')
